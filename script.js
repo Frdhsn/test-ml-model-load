f4745057e9080e06b01f5df772b539c667fedd0c
frdhsn <farhadiitju@gmail.com>
2022-09-13 19:43:13 +0600

    comment added

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 import * as tf from '@tensorflow/tfjs';
 import myJson from "./model/model.json" assert { type: 'json' };
 async function go() {
+    // the following line works fine
     //const model = await tf.loadLayersModel('https://storage.googleapis.com/tfjs-models/tfjs/mnist_transfer_cnn_v1/model.json'); 
+    
+    // this doesn't work. myJson is the model.json file we've saved from google collab
     const mymodel = await tf.loadLayersModel(myJson); 
     mymodel.summary();
 }
